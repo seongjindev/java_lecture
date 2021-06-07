f437204216c7b709cb8ba8e086708120b42570c1
bae <baesj11@gmail.com>
2021-06-07 19:32:27 +0900

    operator

---
+++ src/Operator/OperatorEx.java
@@ -0,0 +1,10 @@
+package Operator;
+
+public class OperatorEx {
+    public static void main(String[] args){
+        int a = 2;
+        int b = 4;
+        System.out.println(a*b);
+        System.out.println(a+b);
+    }
+}
