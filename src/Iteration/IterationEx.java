d872bddfa0e9dfa224861951a01ec6c7898a93e7
bae <baesj11@gmail.com>
2021-06-07 19:32:40 +0900

    iteration

---
+++ src/Iteration/IterationEx.java
@@ -0,0 +1,20 @@
+package Iteration;
+
+public class IterationEx {
+    public static void main(String[] args){
+        int a = 0;
+        int b = 10;
+        for(int i = 0;i<10;i++){
+            System.out.println(i);
+        }
+        while(a<10){
+            System.out.println(a);
+            a++;
+        }
+        do{
+            System.out.println(b);
+            b++;
+        }while(b<10);
+
+    }
+}
